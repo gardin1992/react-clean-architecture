c018eb92198e35a2814b3d6882168cf5661edc89
João Vieira <joao.vieira@marttech.com.br>
2021-08-28 19:17:56 -0300

    add fields form product

--- src/product-management/presentation/views/product-management/index.tsx
+++ src/product-management/presentation/views/product-management/index.tsx
@@ -1,3 +1,4 @@
+import { FomInputCheckbox } from "app/presentation/components/form/input-checkbox-form";
 import { FormInput } from "app/presentation/components/form/input-form";
 
 import React, { useState } from "react";
@@ -6,31 +7,98 @@ import {
   SProductManagementTitle,
 } from "./button-save";
 
-export function ProductManagement() {
-  const [title, setTitle] = useState("012.345.678-90");
+export type ProductFormData = {
+  title: string;
+  description: string;
+  price: number;
+  active: boolean;
+  category: string;
+  code: string;
+};
 
-  function handleOnChange(e: React.FormEvent<HTMLInputElement>) {
-    setTitle(e.currentTarget.value);
+export const initialProductFormData: ProductFormData = {
+  title: "",
+  description: "",
+  price: 0,
+  active: true,
+  category: "",
+  code: "",
+};
+
+export function ProductManagement() {
+  const [productFormData, setProductFormData] = useState(
+    initialProductFormData
+  );
+
+  function handleOnChange<T>(property: string, value: T) {
+    setProductFormData((prev) => ({ ...prev, [property]: value }));
   }
 
   return (
     <div>
       <SProductManagementTitle>
         <h2 className="title">
-          Add Product <small>{title}</small>
+          Add Product <small>{productFormData.title}</small>
         </h2>
       </SProductManagementTitle>
 
       <form>
         <FormInput
           label={"Nome do produto"}
-          value={title}
-          handleOnChange={handleOnChange}
+          value={productFormData.title}
+          handleOnChange={(e) => {
+            const value = e.currentTarget.value;
+            handleOnChange<string>("title", value);
+          }}
         />
         <FormInput
           label={"Descrição"}
           placeholder="Descrição curta do produto."
-          handleOnChange={handleOnChange}
+          value={productFormData.description}
+          handleOnChange={(e) => {
+            const value = e.currentTarget.value;
+            handleOnChange<string>("description", value);
+          }}
+        />
+        <FormInput
+          label={"Preço"}
+          value={String(productFormData.price)}
+          handleOnChange={(e) => {
+            const value = e.currentTarget.value;
+
+            handleOnChange<number>(
+              "price",
+              parseFloat(Number(value).toFixed(2))
+            );
+          }}
+        />
+
+        <FomInputCheckbox
+          label={"Ativo"}
+          value={String(productFormData.active)}
+          handleOnChange={(e) => {
+            const value = e.currentTarget.value;
+            console.log(value);
+            handleOnChange<boolean>("active", true);
+          }}
+        />
+
+        <FormInput
+          label={"Categoria"}
+          value={productFormData.category}
+          handleOnChange={(e) => {
+            const value = e.currentTarget.value;
+            handleOnChange<string>("category", value);
+          }}
+        />
+
+        <FormInput
+          label={"Código"}
+          value={productFormData.code}
+          handleOnChange={(e) => {
+            const value = e.currentTarget.value;
+            handleOnChange<string>("code", value);
+          }}
         />
       </form>
 
